6a172256695b5dec95b73eaf945af809b93e1fdf
Dhruvi45 <78956485+Dhruvi45@users.noreply.github.com>
2024-08-07 17:09:40 +0000

    update login design

--- client/src/component/Login.tsx
+++ client/src/component/Login.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import { Container, Form, Button } from 'react-bootstrap';
-
+import { Container, Form, Button, Row, Col, Card } from 'react-bootstrap';
+import '../css/auth.css'
 interface LoginFormInputs {
   email: string;
   password: string;
@@ -15,9 +15,9 @@ const Login: React.FC = () => {
   const onSubmit = (data: LoginFormInputs) => {
     const users = JSON.parse(localStorage.getItem('users') || '[]');
     const user = users.find((user: any) => user.email === data.email && user.password === data.password);
-    
+
     navigate('/');
-      localStorage.setItem('currentUser', JSON.stringify(user));
+    localStorage.setItem('currentUser', JSON.stringify(user));
     // if (user) {
     // } else {
     //   alert('Invalid email or password');
@@ -25,19 +25,27 @@ const Login: React.FC = () => {
   };
 
   return (
-    <Container>
-      <h2>Login</h2>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" {...register('email', { required: true })} />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" {...register('password', { required: true })} />
-        </Form.Group>
-        <Button variant="primary" type="submit">Login</Button>
-      </Form>
+    <Container className="custom-container">
+      <Row className="justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <Col xs={12}>
+          <Card className="custom-card">
+            <Card.Body>
+              <h2 className="text-center mb-4">Login</h2>
+              <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className='mb-3'>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" {...register('email', { required: true })} />
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" {...register('password', { required: true })} />
+                </Form.Group>
+                <Button variant="primary" type="submit" className="w-100">Login</Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
