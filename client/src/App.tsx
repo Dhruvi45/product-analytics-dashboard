d066a2f4e052cb7340e49b1e7d8f252c7704d2fd
SYNOVERGETECH\joshidhr <dhruvi.joshi@synoverge.com>
2024-08-16 12:21:40 +0530

    route changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
 import Dashboard from './component/Dashboard';
-import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router ,Routes, Route, Navigate } from 'react-router-dom';
 import Login from './component/Auth';
 function App() {
   return (
     <>
      <Router>
        <Routes>
-         <Route path="/" element={<Dashboard />} />
          <Route path="/login" element={<Login />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/" element={<Navigate to="/dashboard" />} />
        </Routes>
      </Router>
     </>
